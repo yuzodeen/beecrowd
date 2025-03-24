0a07975240ae21bee53d657bc3c26e497c1d4909
Ryan <ryan.silva7960@gmail.com>
2025-03-24 09:45:50 -0300

    feat: imprime hello world

---
+++ java/Beecrowd1000/Main.java
@@ -0,0 +1,9 @@
+package java.Beecrowd1000;
+public class Main{
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
+
+//Obs: Foi necessário colocar "Main" na classe para que conseguisse ser aceito pelo Beecrowd
+//Obs: I needed to name the class "Main" for it to be accepted by Beecrowd
